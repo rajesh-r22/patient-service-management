fd7bbac729ae4aab1fa50b65488bd2717f5876e2
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-01-17 04:45:51 +0530

    feat:Created model package for entities and Added new Patient class with added vriables

---
+++ src/main/java/com/pm/patient_service/model/Patient.java
@@ -0,0 +1,38 @@
+package com.pm.patient_service.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registerDate;
+}
