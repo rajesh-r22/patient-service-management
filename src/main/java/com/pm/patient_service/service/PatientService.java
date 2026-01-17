054ef40b8adaec4a750f6a7bd6d1ef15e349462d
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-01-18 04:12:10 +0530

    feat:getPatients logic in service layer

--- src/main/java/com/pm/patient_service/service/PatientService.java
+++ src/main/java/com/pm/patient_service/service/PatientService.java
@@ -1,5 +1,7 @@
 package com.pm.patient_service.service;
 
+import com.pm.patient_service.dto.PatientResponseDto;
+import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +14,16 @@ import java.util.List;
 public class PatientService {
     private final PatientRepository patientRepo;
 
-    public List<PatientResponseDto> getpatients(){
+//    logic to get patients list
+    public List<PatientResponseDto> getPatients(){
+
+//       first get all patients from patientRepo(entity)
         List<Patient> patients=patientRepo.findAll();
+
+//       convert it to dto response
+        List<PatientResponseDto> patientResponseDtos=patients.stream()
+                .map(patient -> PatientMapper.toDto(patient)).toList();
+//        return to client
+        return patientResponseDtos;
     }
 }
