87905cbb50dda5d0d0c974c6a718e0d48376e37b
rajesh-r22 <itsmerajeshreddy01@gmail.com>
2026-01-18 03:31:53 +0530

    feat:Add Service layer to write logic of patient

---
+++ src/main/java/com/pm/patient_service/service/PatientService.java
@@ -0,0 +1,14 @@
+package com.pm.patient_service.service;
+
+import com.pm.patient_service.model.Patient;
+import com.pm.patient_service.repository.PatientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PatientService {
+    private final PatientRepository patientRepo;
+}
